b8aade2afe16052bf22c2588345549187b1efa66
GCharan-Kanini <Charan.Girish@kanini.com>
2026-09-02 13:42:04 +0530

    chore: seed importable todo_app stubs for TDD bootstrap

---
+++ todo_app/todo.py
@@ -0,0 +1,17 @@
+"""Todo list module — stubs to be implemented."""
+
+
+class Todo:
+    def __init__(self, title):
+        raise NotImplementedError
+
+
+class TodoList:
+    def add(self, title):
+        raise NotImplementedError
+
+    def complete(self, index):
+        raise NotImplementedError
+
+    def list_all(self):
+        raise NotImplementedError
